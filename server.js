ca5dc8d8650cebdd721f4d375a7f850e88e9a853
montalvocanojacquelinem3s2-cyber <montalvo.cano.jacqueline.m3s2@gmail.com>
2026-04-22 16:18:13 -0600

    Actualizar server.js

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ const Device = mongoose.model("Device", {
   lastUp: String
 });
 
+const User = mongoose.model("User", {
+  email: String,
+  password: String
+});
+
 // GUARDAR DATOS
 app.post("/devices", async (req, res) => {
   try {
@@ -44,6 +49,85 @@ app.get("/devices", async (req, res) => {
     res.json(devices);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Faltan datos"
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "El usuario ya existe"
+      });
+    }
+
+    const newUser = new User({ email, password });
+    await newUser.save();
+
+    res.json({
+      success: true,
+      message: "Usuario registrado"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Faltan datos"
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Usuario no encontrado"
+      });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({
+        success: false,
+        message: "Contraseña incorrecta"
+      });
+    }
+
+    res.json({
+      success: true,
+      user: {
+        id: user._id,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
   }
 });
 
