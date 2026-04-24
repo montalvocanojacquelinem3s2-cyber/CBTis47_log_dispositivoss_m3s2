f480d09eeec7e8229780a4a601821a8881bef0ed
montalvocanojacquelinem3s2-cyber <montalvo.cano.jacqueline.m3s2@gmail.com>
2026-04-23 23:53:45 -0600

    Actualizar server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // conexión con MongoDB 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log(err));
 
